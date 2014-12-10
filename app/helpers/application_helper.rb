9bebbb2900fe87a56b738c82b19457ed30fa5f48
benjamin <silva96@gmail.com>
2014-12-09 21:31:30 -0300

    ultra basic layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def devise_message_to_twbs(message_type)
+    if message_type == 'notice'
+      type = "success"
+    elsif message_type == 'alert'
+      type = "danger"
+    else
+      type = message_type
+    end
+    return type
+  end
+
 end
