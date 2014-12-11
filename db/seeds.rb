e638eeb146da552894f19984f7ef66f848ef7f3e
benjamin <silva96@gmail.com>
2014-12-11 20:11:43 -0300

    adding extra survey for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,4 +30,15 @@ Type.find_or_create_by(name: 'free-text')
     Answer.create(question: q, value: "textarea")
   end
 end
-
+s = Survey.find_or_create_by(name: 'T-Mobile') do |survey|
+  survey.code = "tmobXz123le"
+  survey.description = "This is a test survey for T-Mobile"
+  survey.active_from = "2014-12-10 00:00:00"
+  survey.active_until = "2015-04-01 01:25:00"
+  survey.public = true
+end
+sect=Section.create(order: n, name: "Step 1", survey: s)
+Question.create(section: sect, type: Type.find_by_name('satisfaction-100'), text: "Overall satisfaction of service:")
+Question.create(section: sect, type: Type.find_by_name('satisfaction-100'), text: "Professionalism and Courtesy of employee")
+Question.create(section: sect, type: Type.find_by_name('satisfaction-100'), text: "How satisfied were you with the wait time at this store?")
+Question.create(section: sect, type: Type.find_by_name('free-text'), text: "Please provide extra comments")
