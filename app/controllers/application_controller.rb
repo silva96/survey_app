4cc59af722452559cd168c468688630294f207e6
benjamin <silva96@gmail.com>
2014-12-10 23:43:08 -0300

    Internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_locale
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_path, :alert => exception.message
   end
+  private
+  def set_locale
+    I18n.locale = params[:lang] || I18n.default_locale
+  end
+
+
 end
